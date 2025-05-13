764709c86b1cff74a72848b46e6f19343202c848
Tavinho09-LO <moreiraalvesluizotavio001@gmail.com>
2025-05-13 18:58:03 +0000

    terminei o projeto

---
+++ carta1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+    int população;
+    float área;
+    float PIB;
+    int pontosturísticos;
+    //informações da carta1//
+
+    printf("Total de habitantes:\n");
+    scanf("%d",&população);
+
+    printf("Área em km2:\n");
+    scanf("%f",&área);
+
+    printf("PIB:\n");
+    scanf("%f",PIB);
+
+    printf("Quantidade de pontos turísticos:\n");
+    scanf("%d",&pontosturísticos);
+    //entrada e saida de dados//
+
+
+    printf("Carta1!\n");
+    printf("Habitantes:%d\n",população);
+    printf("Área em km2:%.2f\n",área);
+    printf("PIB:%.2f\n",PIB);
+    printf("Pontos turísticos:%d\n",pontosturísticos);
+    //imprimindo as variaveis//
+
+    return 0
+
+}
